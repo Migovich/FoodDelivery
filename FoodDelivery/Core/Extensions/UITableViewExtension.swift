74bde815605680199335522bf07b7010b9a66b7b
Migovich <migovich93@gmail.com>
2019-03-16 16:30:11 +0200

    Dequeing and registering reusable view

---
+++ FoodDelivery/Core/Extensions/UITableViewExtension.swift
@@ -0,0 +1,49 @@
+//
+//  UITableViewExtension.swift
+//  FoodDelivery
+//
+//  Created by Myhovych on 3/14/19.
+//  Copyright © 2019 Artem Migovich. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    //Registering Cell
+    func register<T: UITableViewCell>(_: T.Type) where T: ReusableView {
+        register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func register<T: UITableViewCell>(_: T.Type) where T: ReusableView, T: NibLoadableView {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    //Registering HeaderFooterView
+    func register<T: UITableViewHeaderFooterView>(_: T.Type) where T: ReusableView {
+        register(T.self, forHeaderFooterViewReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func register<T: UITableViewHeaderFooterView>(_: T.Type) where T: ReusableView, T: NibLoadableView {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forHeaderFooterViewReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    //Dequeue methods for UITableView
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T where T: ReusableView {
+        guard let cell = dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return cell
+    }
+    
+    func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(_ : T.Type) -> T where T: ReusableView {
+        guard let headerFooter = dequeueReusableHeaderFooterView(withIdentifier: T.defaultReuseIdentifier) as? T else {
+            fatalError("Could not dequeue Header/Footer with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return headerFooter
+    }
+}
---
+++ FoodDelivery/Other/ReusableView.swift
@@ -0,0 +1,29 @@
+//
+//  ReusableView.swift
+//  FoodDelivery
+//
+//  Created by Myhovych on 3/14/19.
+//  Copyright © 2019 Artem Migovich. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+protocol NibLoadableView: class {
+    static var nibName: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension NibLoadableView where Self: UIView {
+    static var nibName: String {
+        return String(describing: self)
+    }
+}
